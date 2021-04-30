c67f065ffd45fc1762118a8b24a94fc013d38965
pauloalchemist <paulolinsdev@gmail.com>
2021-04-30 19:46:02 -0300

    connection string postgres

--- src/database/connectionDb.js
+++ src/database/connectionDb.js
@@ -1,1 +1,14 @@
-import pg from 'pg'
+import { Client } from 'pg'
+require('dotenv').config()
+
+const db = new Client({
+  connectionString: process.env.DATABASE_URL
+})
+
+db.connect()
+  .then(() => console.log('Database connected with success!'))
+  .catch(err => console.error('Connection database error', err.stack))
+
+const query = (text, params) => db.query(text, params)
+
+export { query }
